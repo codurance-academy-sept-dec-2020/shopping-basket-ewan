ee4f1a18e62e12bada380b2f1629c64b230c1248
ewansheldon <ewansheldon@gmail.com>
2020-09-23 13:47:33 +0100

    fixes acceptance test for full basket functionality

--- src/main/java/services/ShoppingBasketService.java
+++ src/main/java/services/ShoppingBasketService.java
@@ -1,5 +1,6 @@
 package services;
 
+import entities.ProductID;
 import entities.ShoppingBasket;
 import entities.UserID;
 import exceptions.NoBasketException;
@@ -14,5 +15,9 @@ public class ShoppingBasketService {
 
     public ShoppingBasket basketFor(UserID userId) throws NoBasketException {
         return shoppingBasketRepository.getBasketFor(userId);
+    }
+
+    public void addItem(UserID userId, ProductID productID, int quanity) {
+        throw new UnsupportedOperationException();
     }
 }
--- src/test/java/acceptance/ShoppingBasketTest.java
+++ src/test/java/acceptance/ShoppingBasketTest.java
@@ -1,6 +1,7 @@
 package acceptance;
 
 import entities.OrderProduct;
+import entities.ProductID;
 import entities.ShoppingBasket;
 import entities.UserID;
 import exceptions.NoBasketException;
@@ -16,6 +17,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ShoppingBasketTest {
     private static final UserID USER_ID = new UserID();
+    private static final ProductID HOBBIT_PRODUCT_ID = new ProductID();
+    private static final ProductID BREAKING_BAD_PRODUCT_ID = new ProductID();
 
     @Test
     void should_not_create_a_shopping_basket_before_user_has_added_an_item() {
@@ -30,17 +33,21 @@ public class ShoppingBasketTest {
         ShoppingBasketService shoppingBasketService = new ShoppingBasketService(shoppingBasketRepository);
         LocalDate createdDate = LocalDate.of(2020, 9, 22);
 
+        int hobbitQuantity = 2;
+        int breakingBadQuantity = 5;
+        shoppingBasketService.addItem(USER_ID, HOBBIT_PRODUCT_ID, hobbitQuantity);
+        shoppingBasketService.addItem(USER_ID, BREAKING_BAD_PRODUCT_ID, breakingBadQuantity);
         ShoppingBasket basket = shoppingBasketService.basketFor(USER_ID);
 
         assertEquals(2, basket.getProducts().size());
 
         OrderProduct hobbit = basket.getProducts().get(0);
-        assertEquals(2, hobbit.getQuantity());
+        assertEquals(hobbitQuantity, hobbit.getQuantity());
         assertEquals(5, hobbit.getProductUnitCost());
         assertEquals(10, hobbit.getOrderProductTotal());
 
         OrderProduct breakingBad = basket.getProducts().get(1);
-        assertEquals(5, breakingBad.getQuantity());
+        assertEquals(breakingBadQuantity, breakingBad.getQuantity());
         assertEquals(7, breakingBad.getProductUnitCost());
         assertEquals(14, breakingBad.getOrderProductTotal());
 
